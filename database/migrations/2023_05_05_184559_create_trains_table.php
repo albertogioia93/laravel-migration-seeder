d9512d7932bd54595ae62509ca6fd864001e21a8
albertogioia93 <albertogioia@hotmail.it>
2023-05-05 21:28:28 +0200

    creazione dati in migrations

--- database/migrations/2023_05_05_184559_create_trains_table.php
+++ database/migrations/2023_05_05_184559_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->time('orario_di_partenza', 12);
+            $table->time('orario_di_arrivo ', 12);
+            $table->string('codice_treno', 12);
+            $table->smallInteger('numero_carrozze')->unsigned();
+            $table->tinyInteger('in_orario', 1);
+            $table->tinyInteger('cancellato', 1);
             $table->timestamps();
         });
     }
